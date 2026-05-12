c0dce4ac673b658e72a28572c0c18146a5e5afba
chamikara12 <chamikara12@gmail.com>
2026-05-12 21:26:17 +0530

    email logic added

--- src/main/java/com/vau/UniMerch/UniMerch/controller/AuthController.java
+++ src/main/java/com/vau/UniMerch/UniMerch/controller/AuthController.java
@@ -1,13 +1,17 @@
 package com.vau.UniMerch.UniMerch.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.vau.UniMerch.UniMerch.Security.JwtUtils;
 import com.vau.UniMerch.UniMerch.dto.req.LoginRequest;
 import com.vau.UniMerch.UniMerch.dto.res.AuthResponse;
 import com.vau.UniMerch.UniMerch.model.User;
 import com.vau.UniMerch.UniMerch.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/auth")
@@ -45,6 +49,9 @@ public class AuthController {
     public AuthResponse login(@RequestBody LoginRequest loginData) {
 
         User user = userRepo.findByEmail(loginData.getEmail());
+        //usually email are case-insensitive, so we can convert it to lower case before checking
+       // User user = userRepo.findByEmail(loginData.getEmail().toLowerCase());
+    
 
         if (user != null && encoder.matches(loginData.getPassword(), user.getPassword())) {
             String token = jwtUtils.generateToken(user.getEmail());
