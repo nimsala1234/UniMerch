45426f7a3b2261c31a77fc599489a90b1d59c373
chamikara12 <chamikara12@gmail.com>
2026-05-12 21:17:23 +0530

    the email verification has been updated

--- src/main/java/com/vau/UniMerch/UniMerch/controller/AuthController.java
+++ src/main/java/com/vau/UniMerch/UniMerch/controller/AuthController.java
@@ -5,7 +5,6 @@ import com.vau.UniMerch.UniMerch.dto.req.LoginRequest;
 import com.vau.UniMerch.UniMerch.dto.res.AuthResponse;
 import com.vau.UniMerch.UniMerch.model.User;
 import com.vau.UniMerch.UniMerch.repository.UserRepository;
-import com.vau.UniMerch.UniMerch.Security.JwtUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.*;
@@ -23,16 +22,35 @@ public class AuthController {
     @Autowired
     private BCryptPasswordEncoder encoder;
 
+    @PostMapping("/register")
+    public String register(@RequestBody User user) {
+        String email = user.getEmail().toLowerCase();
+
+        if (!email.endsWith("@stu.vau.ac.lk") && !email.endsWith("@vau.ac.lk")) {
+            throw new RuntimeException("Only @stu.vau.ac.lk or @vau.ac.lk emails are allowed!");
+        }
+
+        if (userRepo.findByEmail(email) != null) {
+            throw new RuntimeException("Email is already registered!");
+        }
+
+        user.setRole(User.Role.STUDENT_STAFF);
+        user.setPassword(encoder.encode(user.getPassword()));
+
+        userRepo.save(user);
+        return "User registered successfully!";
+    }
+
     @PostMapping("/login")
     public AuthResponse login(@RequestBody LoginRequest loginData) {
+
         User user = userRepo.findByEmail(loginData.getEmail());
 
-        // Compare the raw password from request with hashed password in DB
         if (user != null && encoder.matches(loginData.getPassword(), user.getPassword())) {
             String token = jwtUtils.generateToken(user.getEmail());
             return new AuthResponse(token, user.getRole().toString(), user.getEmail());
         }
 
-        throw new RuntimeException("Invalid email  password");
+        throw new RuntimeException("Invalid email or password");
     }
 }
